62d05429aac9d209a45408cbb400f6c5d90aef0e
Richer Balázs <richer.balazs@gmail.com>
2017-10-06 20:25:33 +0200

    Separate current value and config from main logic.

--- src/app.js
+++ src/app.js
@@ -5,30 +5,32 @@
  */
 let Application = function () {
     /**
+     * @type {Application}
+     */
+    let self = this;
+
+    /**
+     * @type {number}
+     */
+    this.value = 100;
+
+    /**
      * Provides the response.
      */
     this.execute = function () {
-        let value = 100;
-        let config1 = liquidFillGaugeDefaultSettings();
+        let gauge = loadLiquidFillGauge("gauge", 28, self.getConfig());
 
-        config1.circleThickness = 0.2;
-        config1.textVertPosition = 0.3;
-        config1.waveAnimateTime = 1000;
-        config1.textSize = 0.6;
-
-        let gauge = loadLiquidFillGauge("gauge", 28, config1);
-
-        gauge.update(value);
+        gauge.update(self.value);
 
         let callback = function () {
-            value -= Math.floor(Math.random() * 6) + 1;
-            value = value < 0 ? 0 : value;
+            self.value -= Math.floor(Math.random() * 6) + 1;
+            self.value = self.value < 0 ? 0 : self.value;
 
-            gauge.update(value);
+            gauge.update(self.value);
 
             setTimeout(function () {
                 callback()
-            }, (Math.random() * (14000 - 2500) + 2500) + (value + 50) * 100);
+            }, (Math.random() * (14000 - 2500) + 2500) + (self.value + 50) * 100);
         };
 
         setTimeout(function () {
@@ -37,11 +39,22 @@ let Application = function () {
 
         document.body.onkeyup = function (e) {
             if (e.keyCode === 32) {
-                value = 100;
-                gauge.update(value);
+                self.value = 100;
+                gauge.update(self.value);
             }
         }
-    }
+    };
+
+    this.getConfig = function () {
+        let config1 = liquidFillGaugeDefaultSettings();
+
+        config1.circleThickness = 0.2;
+        config1.textVertPosition = 0.3;
+        config1.waveAnimateTime = 1000;
+        config1.textSize = 0.6;
+
+        return config1;
+    };
 };
 
 module.exports = Application;
